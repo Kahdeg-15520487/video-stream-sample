d5a8b603d67eecf60b8b4573472f99733b435781
Clark Weckmann <cweckmaniii@gmail.com>
2017-11-26 14:11:29 -0600

    + Error and Mount-Utils

--- server.js
+++ server.js
@@ -3,34 +3,58 @@ const fs = require('fs')
 const path = require('path')
 const app = express()
 const videoJson = require('./videoJson')
+var mountutil = require('linux-mountutils')
 
-app.use(express.static(path.join(__dirname, 'public')))
+// Start Of Actual Code
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'))
-// console.log(videoJson["1973"].path)
+app.use(express.static(path.join(__dirname, 'public'))) // Define Public as Static
+
+// Mount /dev/sdc1 to ~/UsbStick
+
+mountutil.mount("/dev/sda1", "~/UsbStick", {
+  "createDir": false
+}, function (result) {
+  if (result.error) {
+    // If error show error.html
+    app.get('/', function (req, res) {
+      res.sendFile(path.join(__dirname +
+        '/public/error.html'))
+    })
+    console.log(result.error);
+  } else {
+    // mount succeeded
+    app.get('/', function (req, res) {
+      res.sendFile(path.join(__dirname +
+        '/index.html'))
+      // console.log(videoJson["1973"].path)
+    })
+  }
 })
-
-app.get('/play/:VIDEO', function(req, res) {
-var videoID = req.params.VIDEO.toString();
-  var path = "/home/clarkhacks/UsbStick/movies" + videoJson[videoID].path
+// Get Video To Play
+app.get('/play/:VIDEO', function (req, res) {
+  var videoID = req.params.VIDEO.toString();
+  var path = "/home/clarkhacks/UsbStick/movies" + videoJson[videoID].path // Path To Movies
   const stat = fs.statSync(path)
   const fileSize = stat.size
   const range = req.headers.range
   if (range) {
     const parts = range.replace(/bytes=/, "").split("-")
     const start = parseInt(parts[0], 10)
-    const end = parts[1]
-      ? parseInt(parts[1], 10)
-      : fileSize-1
+    const end = parts[1] ?
+      parseInt(parts[1], 10) :
+      fileSize - 1
 
-    const chunksize = (end-start)+1
-    const file = fs.createReadStream(path, {start, end})
+    const chunksize = (end - start) +
+      1
+    const file = fs.createReadStream(path, {
+      start,
+      end
+    })
     const head = {
       'Content-Range': `bytes ${start}-${end}/${fileSize}`,
       'Accept-Ranges': 'bytes',
       'Content-Length': chunksize,
-      'Content-Type': 'video/mp4',
+      'Content-Type': 'video/mp4'
     }
 
     res.writeHead(206, head)
@@ -38,7 +62,7 @@ var videoID = req.params.VIDEO.toString();
   } else {
     const head = {
       'Content-Length': fileSize,
-      'Content-Type': 'video/mp4',
+      'Content-Type': 'video/mp4'
     }
     res.writeHead(200, head)
     fs.createReadStream(path).pipe(res)
