28a2e5227d31e1652da5bc41d9739f3871792b5d
kahdeg <15520487@gm.uit.edu.vn>
2018-10-02 20:05:57 +0700

    Add collabsible video list

--- Program.cs
+++ Program.cs
@@ -13,6 +13,63 @@ namespace video_stream_sample
     }
     class Program
     {
+        const string templateHead = 
+@"<html>
+<head>
+<meta name=""viewport"" content=""width=device-width, initial-scale=1"">
+<style>
+.collapsible {
+    background-color: #777;
+    color: white;
+    cursor: pointer;
+    padding: 18px;
+    width: 100%;
+    border: none;
+    text-align: left;
+    outline: none;
+    font-size: 15px;
+}
+
+.active, .collapsible:hover {
+    background-color: #555;
+}
+
+.content {
+    padding: 0 18px;
+    display: none;
+    overflow: hidden;
+    background-color: #f1f1f1;
+}
+</style>
+</head>
+<body>";
+        const string templateDiv =
+@"
+<button class=""collapsible"">{0}</button>
+<div class=""content"">
+";
+        const string templateEnd =
+@"
+<script>
+var coll = document.getElementsByClassName(""collapsible"");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener(""click"", function() {
+    this.classList.toggle(""active"");
+    var content = this.nextElementSibling;
+    if (content.style.display === ""block"") {
+      content.style.display = ""none"";
+    } else {
+      content.style.display = ""block"";
+    }
+  });
+}
+</script>
+
+</body>
+</html>
+";
         static void Main(string[] args)
         {
             Dictionary<string,VideoConfig> VideoConfigs = new Dictionary<string, VideoConfig>();
@@ -20,10 +77,11 @@ namespace video_stream_sample
             int id = 1000;
             using (StringWriter sw = new StringWriter(videoList))
             {
-                sw.WriteLine("<html>");
+                sw.WriteLine(templateHead);
                 foreach (var arg in args)
                 {
                     if(Directory.Exists(arg)){
+                        sw.WriteLine(templateDiv,arg);
                         foreach (var file in Directory.EnumerateFiles(arg,"*.mp4",SearchOption.TopDirectoryOnly))
                         {
                             var video = new VideoConfig(){
@@ -35,9 +93,10 @@ namespace video_stream_sample
                             sw.WriteLine($"<a href=\"/play/{id}\">{video.name}</a><br/>");
                             id++;
                         }
+                        sw.WriteLine("</div>");
                     }
                 }
-                sw.WriteLine("</html>");
+                sw.WriteLine(templateEnd);
             }
             File.WriteAllText("videoConfig.json",JsonConvert.SerializeObject(VideoConfigs,Formatting.Indented));
             File.WriteAllText("videoList.html",videoList.ToString());
