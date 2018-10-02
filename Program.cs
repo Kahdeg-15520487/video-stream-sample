5dcb3d33a6e25dfb66bfe664dafd8e640e53e3ff
kahdeg <15520487@gm.uit.edu.vn>
2018-10-02 13:48:25 +0700

    add c# video list generator

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace video_stream_sample
+{
+    class VideoConfig{
+        public string path{get;set;}
+        public string name{get;set;}
+        public string desc{get;set;}
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string,VideoConfig> VideoConfigs = new Dictionary<string, VideoConfig>();
+            StringBuilder videoList = new StringBuilder();
+            int id = 1000;
+            using (StringWriter sw = new StringWriter(videoList))
+            {
+                sw.WriteLine("<html>");
+                foreach (var arg in args)
+                {
+                    if(Directory.Exists(arg)){
+                        foreach (var file in Directory.EnumerateFiles(arg,"*.mp4",SearchOption.TopDirectoryOnly))
+                        {
+                            var video = new VideoConfig(){
+                                path = file,
+                                name = Path.GetFileNameWithoutExtension(file),
+                                desc = "lorem"
+                            };
+                            VideoConfigs.Add(id.ToString(),video);
+                            sw.WriteLine($"<a href=\"/play/{id}\">{video.name}</a><br/>");
+                            id++;
+                        }
+                    }
+                }
+                sw.WriteLine("</html>");
+            }
+            File.WriteAllText("videoConfig.json",JsonConvert.SerializeObject(VideoConfigs,Formatting.Indented));
+            File.WriteAllText("videoList.html",videoList.ToString());
+        }
+    }
+}
--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express')
 const fs = require('fs')
 const path = require('path')
 const app = express()
-const videoJson = require('./videoJson')
+//const videoJson = require('./videoJson')
+const videoJson = require('./videoConfig')
 var mountutil = require('linux-mountutils')
 
 // Start Of Actual Code
@@ -12,48 +13,51 @@ app.engine('pug', require('pug').__express)
 app.set("view engine", "pug")
 app.set("views", path.join(__dirname, "views"))
 app.get('/', function (req, res) {
-res.render("home")
+  res.sendFile(__dirname + '/videoList.html')
 })
 // Get Video To Play
 app.get('/play/:VIDEO', function (req, res) {
   var videoID = req.params.VIDEO.toString();
-if (videoJson.hasOwnProperty(videoID)) {
-    var path = "/home/clarkhacks/UsbStick/movies" + videoJson[videoID].path // Path To Movies
-    const stat = fs.statSync(path)
-    const fileSize = stat.size
-    const range = req.headers.range
-    if (range) {
-      const parts = range.replace(/bytes=/, "").split("-")
-      const start = parseInt(parts[0], 10)
-      const end = parts[1] ?
-        parseInt(parts[1], 10) :
-        fileSize - 1
+  if (videoJson.hasOwnProperty(videoID)) {
+      //var path = "/home/clarkhacks/UsbStick/movies" + videoJson[videoID].path // Path To Movies
+      var path = videoJson[videoID].path // Path To Movies
+      const stat = fs.statSync(path)
+      const fileSize = stat.size
+      const range = req.headers.range
+      if (range) {
+        const parts = range.replace(/bytes=/, "").split("-")
+        const start = parseInt(parts[0], 10)
+        const end = parts[1] ?
+          parseInt(parts[1], 10) :
+          fileSize - 1
 
-      const chunksize = (end - start) +
-        1
-      const file = fs.createReadStream(path, {
-        start,
-        end
-      })
-      const head = {
-        'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-        'Accept-Ranges': 'bytes',
-        'Content-Length': chunksize,
-        'Content-Type': 'video/mp4'
-      }
+        const chunksize = (end - start) +
+          1
+        const file = fs.createReadStream(path, {
+          start,
+          end
+        })
+        const head = {
+          'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+          'Accept-Ranges': 'bytes',
+          'Content-Length': chunksize,
+          'Content-Type': 'video/mp4'
+        }
 
-      res.writeHead(206, head)
-      file.pipe(res)
-    } else {
-      const head = {
-        'Content-Length': fileSize,
-        'Content-Type': 'video/mp4'
+        res.writeHead(206, head)
+        file.pipe(res)
+      } else {
+        const head = {
+          'Content-Length': fileSize,
+          'Content-Type': 'video/mp4'
+        }
+        res.writeHead(200, head)
+        fs.createReadStream(path).pipe(res)
       }
-      res.writeHead(200, head)
-      fs.createReadStream(path).pipe(res)
-    }
-  } else {
-res.sendFile(__dirname + '/public/error.html')
+  } 
+  else 
+  {
+    res.sendFile(__dirname + '/public/error.html')
   }
 })
 
